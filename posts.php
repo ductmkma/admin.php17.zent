b587e0b188a1b24eeac99aef359b2f4e1fc38572
ductmkma <ductm.kma@gmail.com>
2018-12-15 10:34:21 +0700

    up

---
+++ posts.php
@@ -0,0 +1,25 @@
+    <?php 
+      require_once("header.php");
+     ?>
+
+    <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+          <!-- Breadcrumbs-->
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="index.html">Bảng điều khiển</a>
+            </li>
+            <li class="breadcrumb-item active">Quản lý bài viết</li>
+          </ol>
+
+          <!-- Page Content -->
+          <h1>Quản lý bài viết</h1>
+          <hr>
+          
+        </div>
+        <!-- /.container-fluid -->
+        <?php 
+      require_once("footer.php");
+     ?>
